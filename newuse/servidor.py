06dee4d680fbdb7c3b3bd420916ab5d16b785877
Ronne <macenaronne@gmail.com>
2025-09-03 07:19:33 -0300

    Add files via upload

--- newuse/servidor.py
+++ newuse/servidor.py
@@ -3,16 +3,49 @@ from flask import *
 app = Flask(__name__)
 
 roupas = [['Cuecão', 'cueca', 'G', '12', 'éssa bvosta ai']]
+calcados = []
 vendedores = []
 clientes = []
 logado = False
 cliente = False
 vendedor = False
+nomevendedor = ""
 
 
 @app.route('/')
 def pag_principal():
     return render_template('paginainicial.html')
+
+@app.route('/cadastrarcalcado', methods=['post'])
+def cadastro_calcado():
+
+    global calcados 
+
+    if  logado and vendedor:
+
+        nome = request.form.get('nomecalcado')
+        categoria = request.form.get('categoria')
+        numeracao = request.form.get('numeracao')
+        preco = request.form.get('preco')
+        descricao = request.form.get('descricao')
+
+        if nome == None or nome == '' or preco == None or preco == '':
+    
+            msg = 'Preencha todos os campos!'
+            print(calcados)
+            return render_template('cadastrarcalcado.html', erro = msg)
+
+
+        calcado = [nome, categoria, numeracao, preco, descricao, nomevendedor]
+
+        
+        calcados.append(calcado)
+
+        feedback = 'Calçado cadastrado com sucesso!'
+        print(calcados)
+        return render_template('cadastrarcalcado.html', retorno = feedback)
+    
+    else: return render_template('login.html')
 
 @app.route('/cadastro')
 def cadastro():
@@ -61,12 +94,6 @@ def cadastrar():
         return render_template('cadastrousuario.html', saida = msg)
 
 
-
-
-
-
-
-
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -75,51 +102,58 @@ def login():
 def logar ():
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
-    global logado, cliente, vendedor
+    global logado, cliente, vendedor, nomevendedor
     for cliente in clientes:
         if usuario == cliente[0] and senha == cliente[1]:
             logado = True
             cliente = True
             vendedor = False
-            break
-        else:
-            for vendedor in vendedores:
-                if vendedor == vendedores[0] and senha == vendedor[1]:
-                    logado = True
-                    vendedor = True
-                    cliente = False
-                    break
-
-
-    if logado:
-        return render_template('paginainicial.html')
+            return render_template('paginainicial.html')
+        
+    for vendedor in vendedores:
+        if usuario == vendedor[0] and senha == vendedor[1]:
+            logado = True
+            vendedor = True
+            cliente = False
+            nomevendedor = usuario
+            return render_template('paginainicial.html')
 
     msg = 'Usuário ou senha incorretos'
-    return render_template('login.html', erro=msg)
+    return render_template('login.html', erro = msg)
 
 @app.route('/cadastraritem')
 def cadastro_item():
+    
+    if logado and vendedor:
 
-    return render_template('cadastraritem.html')
+        return render_template('cadastraritem.html')
+    
+    else: return render_template('login.html')
 
 @app.route('/cadastrarroupa', methods=['post'])
 def cadastro_roupa():
 
+    if logado and vendedor:
+        nome = request.form.get('nomeroupa')
+        categoria = request.form.get('categoria')
+        tamanho = request.form.get('tamanho')
+        preco = request.form.get('preco')
+        descricao = request.form.get('descricao')
+        global roupas
 
-    nome = request.form.get('nomeroupa')
-    categoria = request.form.get('categoria')
-    tamanho = request.form.get('tamanho')
-    preco = request.form.get('preco')
-    descricao = request.form.get('descricao')
+        if nome == None or preco == None:
+    
+            msg = 'Preencha todos os campos!'
+            print(roupas)
+            return render_template('cadastrarroupa.html', erro = msg)
 
+        roupa = [nome, categoria, tamanho, preco, descricao, nomevendedor]
 
-    roupa = [nome, categoria, tamanho, preco, descricao]
+        roupas.append(roupa)
+        msg = "Roupa cadastrada com sucesso!"
 
-    global roupas
-    roupas.append(roupa)
-
-    print(roupas)
-    return render_template('cadastrarroupa.html')
+        print(roupas)
+        return render_template('cadastrarroupa.html', retorno = msg)
 
 
 if __name__ == '__main__':
